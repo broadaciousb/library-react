906bd71f9633d3bfa8e174d7013753fded6cd701
Broady <broadybmx@gmail.com>
2024-11-19 20:52:37 -0500

    feat: Add featured, highlight, and book components to reuse in other components, some javascript for fitlering books from data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Nav from './components/Nav.jsx';
 import Landing from './components/Landing.jsx';
 import Highlights from './components/Highlights.jsx';
+import Featured from './components/Featured.jsx';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Nav />
       <Landing />
       <Highlights />
+      <Featured />
     </div>
   );
 }
