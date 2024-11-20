6c354f18fc95606acbe71101c3cb27ca5390001a
Broady <broadybmx@gmail.com>
2024-11-19 19:09:56 -0500

    feat: Add highlights section, fix landing header__description classname and menu__backdrop classname

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Nav from './components/Nav';
+import Nav from './components/Nav.jsx';
+import Landing from './components/Landing.jsx';
+import Highlights from './components/Highlights.jsx';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
     </div>
   );
 }
