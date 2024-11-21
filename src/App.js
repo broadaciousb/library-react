e60f7de1da3effb9059ad46675552d31bece8683
Broady <broadybmx@gmail.com>
2024-11-20 21:37:31 -0500

    feat: Make routes in App.js, add Javascript to Books.jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import Nav from './components/Nav.jsx';
-import Landing from './components/Landing.jsx';
-import Highlights from './components/Highlights.jsx';
-import Featured from './components/Featured.jsx';
-import Discounted from './components/Discounted.jsx';
-import Explore from './components/Explore.jsx';
-import Footer from './components/Footer.jsx';
-
+import Nav from "./components/Nav.jsx";
+import Footer from "./components/Footer.jsx";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Books from "./pages/Books.jsx"
+import { books } from "./data.js"
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" render={() => <Books books={books} />} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
