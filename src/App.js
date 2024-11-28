1a79de840cc9d47fa979f5f260d6b0c6bad22f2c
Broady <broadybmx@gmail.com>
2024-11-28 12:35:28 -0500

    feat: Add many component changes, create cart functionality for adding/removing books, adjusting the total etc.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
+import React, { useState, useEffect } from "react";
 import Nav from "./components/Nav.jsx";
 import Footer from "./components/Footer.jsx";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Books from "./pages/Books.jsx";
-
 import BookInfo from "./pages/BookInfo.jsx";
 import { books } from "./data.js";
+import Cart from "./pages/Cart.jsx";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(cart.map((item) => 
+       item.id === book.id
+        ? {
+          ...item,
+          quantity: +quantity,
+        }
+        : item
+      )
+    );
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+    console.log('remove item', item)
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity;
+    })
+
+    return counter;
+  }
+  
+  useEffect(() => {
+    console.log(cart)
+  }, [cart])
+  
+
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav numberOfItems={numberOfItems()}/>
         <Route path="/" exact component={Home} />
         <Route path="/books" exact render={() => <Books books={books} />} />
-        <Route path="/books/1" render={() => <BookInfo books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => <BookInfo books={books} addToCart={addToCart} cart={cart}/>}
+        />
+        <Route path="/cart" render={() => <Cart books={books} cart={cart} changeQuantity={changeQuantity} removeItem={removeItem}/>} />
         <Footer />
       </div>
     </Router>
